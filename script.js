c803096715203ea3a7c4578e3edad95979bc02bf
Zayad Najdaoui <34402772+Najdaoui@users.noreply.github.com>
2024-12-26 21:47:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,14 +13,61 @@ if (todayElement) {
   todayElement.textContent = "(" + formattedDate + ")";
 }
 
-// Sélection de l'élément avec l'ID "sectionTitle"
-const sectionTitle = document.getElementById("sectionTitle");
+function translateTasksTitle() {
+    // Sélection de l'élément avec l'ID "sectionTitle" dans le conteneur Tasks
+    let sectionTitle = document.getElementById("sectionTitle");
 
-// Vérification si l'élément existe
-if (sectionTitle) {
-    // Obtient la hauteur de l'élément
-    const height = sectionTitle.offsetHeight;
+    // Vérification si l'élément existe
+    if (sectionTitle) {
+        // Obtient la hauteur de l'élément
+        const height = sectionTitle.offsetHeight;
 
-    // Applique la hauteur comme une variable CSS
-    sectionTitle.style.setProperty("--translate-y", `${height}px`);
+        // Applique la hauteur comme une variable CSS
+        sectionTitle.style.setProperty("--translate-y", `${height}px`);
+    }
 }
+
+// Appel de la fonction translateTasksTitle
+translateTasksTitle();
+
+// Sélection de tous les éléments de navigation
+const navBtns = document.querySelectorAll("nav .nav-link");
+
+// Sélection du bouton d'action
+let action = document.getElementById("action");
+// Sélection du texte de l'action
+let actionText = document.getElementById("actionText");
+// Sélection de l'attribut data-bs-target de l'élément action
+let actionTarget = action.getAttribute("data-bs-target");
+
+// Ajout d'un écouteur d'événements pour chaque élément de navigation
+for (let btn of navBtns) {
+    btn.addEventListener("click", function(e) {
+
+        // Empêche le comportement par défaut du lien de navigation
+        e.preventDefault();
+
+        // Suppression de la classe active de tous les éléments de navigation
+        navBtns.forEach(btn => btn.classList.remove("active"));
+
+        this.classList.add("active");
+
+        const contentContainers = document.querySelectorAll(".js-container");
+
+        contentContainers.forEach(container => {
+            if (container.id === this.textContent.toLowerCase()) {
+                container.classList.remove("d-none");
+
+                actionText.textContent = "Add " + this.textContent;
+                action.setAttribute("data-bs-target", "#add" + this.textContent + "Modal");
+            } else {
+                container.classList.add("d-none");
+            }
+
+            translateTasksTitle();
+            
+        });
+
+    });
+}
+
